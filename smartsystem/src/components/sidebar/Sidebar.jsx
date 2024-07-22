f5ccaffc367e0d054b0d5352cbb2d9f2939cdcdf
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-07-22 15:38:13 +0530

    sidebar created

---
+++ smartsystem/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+
+const Sidebar = () => {
+    const pathname = useParams();
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Sidebar

43005573cc3f26bb1e375ed6649684f5dba6edb7
Layanga-Rajapakshe <145885562+Layanga-Rajapakshe@users.noreply.github.com>
2024-07-23 18:42:08 +0530

    routes edited

--- smartsystem/src/App.jsx
+++ smartsystem/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Routes, Route } from 'react-router-dom'
-
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -12,24 +11,28 @@ import Company from './pages/super_admin/company'
 import Employee from './pages/super_admin/employee'
 
 export default function App() {
+  const location = useLocation();
+  const showSidebar = ['/company', '/employee'].includes(location.pathname);
+
   return (
     <div>
-        <div className='main-content'>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-
-                <Sidebar>
-                  <Routes>
-                    <Route path="/company" element={<Company />} />
-                    <Route path="/employee" element={<Employee />} />
-                  </Routes>
-                </Sidebar>
-            
-        </div>
-        <Footer />
+      <div className='main-content'>
+        {showSidebar ? (
+          <Sidebar>
+            <Routes>
+              <Route path="/company" element={<Company />} />
+              <Route path="/employee" element={<Employee />} />
+            </Routes>
+          </Sidebar>
+        ) : (
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        )}
+      </div>
+      <Footer />
     </div>
   )
 }

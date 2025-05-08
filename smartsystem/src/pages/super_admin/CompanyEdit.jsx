d5f967e99db6899f4b2fbe92e928779cd1d5685b
Layanga-Rajapakshe <dimutech46@gmail.com>
2025-05-08 08:17:19 +0530

    update8

--- smartsystem/src/pages/super_admin/CompanyEdit.jsx
+++ smartsystem/src/pages/super_admin/CompanyEdit.jsx
@@ -64,7 +64,7 @@ const CompanyEdit = () => {
       const token = localStorage.getItem('token'); // Adjust based on your auth setup
       
       const response = await axios.put(
-        `/api/company/${companyId}`, 
+        `https://smart-system-api.vercel.app/api/company/${companyId}`, 
         companyData,
         {
           headers: {
